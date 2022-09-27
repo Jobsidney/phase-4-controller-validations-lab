febdffc60411c378755328f9c4419fc8f5abb283
Jobsidney <jobsidney67@gmail.com>
2022-09-27 18:45:59 +0300

    commit tree baa1901c39c295169b418017b5022c30e721cde3

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,13 +1,6 @@
 class AuthorsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-
-  def render_not_found_response
-    render json: { error: "author not found" }, status: :not_found
-  end
-  def render_unprocessable_entity_response(invalid)
-    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
-  end
   
   def show
     author = Author.find(params[:id])
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
 
@@ -24,9 +24,9 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:category, :content, :title)
   end
-  def render_not_found_response
-    render json: { error: "author not found" }, status: :not_found
-  end
+  # def render_not_found_response
+  #   render json: { error: "author not found" }, status: :not_found
+  # end
   def render_unprocessable_entity_response(invalid)
     render json: { errors: invalid.record.errors.full_messages.to_sentence}, status: :unprocessable_entity
   end
